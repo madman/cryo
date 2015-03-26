0cff0caa0c3660f79869b35ecc0ca85030020f0a
Yuriy Prokopets <y.prokopets@globo-tech.com>
2015-03-26 20:09:21 +0200

    Clean up authorization

--- app/modules/Core/Provider/SecurityProvider.php
+++ app/modules/Core/Provider/SecurityProvider.php
@@ -14,28 +14,6 @@ class SecurityProvider implements ServiceProviderInterface
     {
         $app->register(new Silex\Provider\SecurityServiceProvider(), [
             'security.firewalls' => [
-                'login_admin'   => [
-                    'pattern'     => '^/login-admin',
-                    'anonymous'   => true,
-                ],
-                'admin'   => [
-                    'pattern'     => '^/admin',
-                    'anonymous'   => false,
-                    'form'        => [
-                        'login_path' => '/login-admin',
-                        'check_path' => '/admin/secured'
-                    ],
-                    'logout'      => [
-                        'logout_path' => '/admin/logout',
-                    ],
-                    'remember_me' => [
-                        'key'                => __FILE__ . 'admin',
-                        'always_remember_me' => true
-                    ],
-                    'users' => $app->share(function () {
-                        return new \Users\Provider\AdminUserProvider;
-                    }),
-                ],
                 'default' => [
                     'pattern'     => '^/',
                     'anonymous'   => true,
@@ -55,29 +33,6 @@ class SecurityProvider implements ServiceProviderInterface
                     }),
                 ]
             ]]);
-
-        $app['security.authentication.success_handler._proto'] = $app->protect(function ($name, $options) use ($app) {
-            return $app->share(function () use ($name, $options, $app) {
-                $handler = new DefaultAuthenticationSuccessHandler(
-                    $app['security.http_utils'],
-                    $options
-                );
-                $handler->setProviderKey($name);
-
-                return $handler;
-            });
-        });
-
-        $app['security.authentication.failure_handler._proto'] = $app->protect(function ($name, $options) use ($app) {
-            return $app->share(function () use ($name, $options, $app) {
-                return new DefaultAuthenticationFailureHandler(
-                    $app,
-                    $app['security.http_utils'],
-                    $options,
-                    $app['logger']
-                );
-            });
-        });
 
         $app->register(new Silex\Provider\RememberMeServiceProvider);
     }
